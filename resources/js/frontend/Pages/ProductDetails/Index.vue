9ba3b1e8f03bf2bbe5235fbf80702f9bba7a218e
Md-shefat-masum <mshefat924@gmail.com>
2024-07-28 17:35:26 +0600

    update details

--- resources/js/frontend/Pages/ProductDetails/Index.vue
+++ resources/js/frontend/Pages/ProductDetails/Index.vue
@@ -96,12 +96,7 @@ export default {
             get_top_products: "get_top_products",
             set_slug: "set_slug",
         }),
-<<<<<<< HEAD
     },
-=======
-
-    // },
->>>>>>> 2d5b010 (ok)
     data: () => ({
         loaded: false,
     }),
