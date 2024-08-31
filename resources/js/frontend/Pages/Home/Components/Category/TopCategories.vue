39ea6d52db27a0c9dfe4429bffe858ffba102cb6
Md-shefat-masum <mshefat924@gmail.com>
2024-08-31 21:16:08 +0600

    load optimize

--- resources/js/frontend/Pages/Home/Components/Category/TopCategories.vue
+++ resources/js/frontend/Pages/Home/Components/Category/TopCategories.vue
@@ -4,7 +4,7 @@
             <div class="section_title">
                 <h4 class="text">Top Offers</h4>
             </div>
-            <div class="d-flex gap-3" v-if="preloader.top_products">
+            <div class="d-flex gap-3" v-if="!all_top_products_offer.length">
                 <skeleton :height="`200px`" :width="`400px`"></skeleton>
                 <skeleton :height="`200px`" :width="`400px`"></skeleton>
                 <skeleton :height="`200px`" :width="`400px`"></skeleton>
@@ -39,43 +39,36 @@
             <div class="section_title">
                 <h4 class="text my-5">Top Categories</h4>
             </div>
-            <div class="d-flex gap-3" v-if="preloader.category_group">
+            <div class="d-flex gap-3" v-if="!all_category_groups.length">
                 <skeleton :height="`200px`" :width="`400px`"></skeleton>
                 <skeleton :height="`200px`" :width="`400px`"></skeleton>
                 <skeleton :height="`200px`" :width="`400px`"></skeleton>
                 <skeleton :height="`200px`" :width="`400px`"></skeleton>
             </div>
             <div class="top_categries" v-else>
-
                 <div v-for="(item, index) in all_category_groups" :key="index" class="category">
                     <Link :href="`/category-group/${item.slug}`" class="category_link">
                     <div class="title"> {{ item.title }}</div>
                     <img :src="load_image(item.image, true)" :alt="`ETEK Top Category ${item.title}`" />
                     </Link>
                 </div>
-
             </div>
         </div>
     </section>
 </template>
 
 <script>
-import { computed, onMounted } from 'vue';
-
 import { use_home_page_store } from '../../Store/home_page_store.js';
 import { mapState } from 'pinia';
 import Skeleton from '../../../../Components/Skeleton.vue';
 export default {
     components: { Skeleton },
-
     data: () => ({
 
     }),
-
     methods: {
         load_image: window.load_image,
     },
-
     computed: {
         ...mapState(use_home_page_store, {
             all_category_groups: 'all_category_groups',
@@ -83,7 +76,6 @@ export default {
             preloader: 'preloader',
         })
     }
-
 }
 </script>
 
